355dd9274e0314fb601c56d903ce152bd7f8d633
Rosalyn Wimberly <rosalynwimberly@gmail.com>
2024-08-09 21:38:44 -0400

    style: Add google font

--- includes/header.php
+++ includes/header.php
@@ -1,20 +1,22 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rosalyn Wimberly</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div id="header">
-        <nav>
-            <div class="logo">
-                <a href="#" class="logo-name">Rosalyn Wimberly</a>
-            </div>
-            <div class="nav-menu">
-                <p>About</p>
-                <p>Contact</p>
-            </div>
+    <div id="header" class="container">
+        <nav >
+            <ul class="nav_links nav_left">
+                <li><a href="#" class="logo">Rosalyn Wimberly</a></li>
+            </ul>
+            <ul class="nav_links nav_right">
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
         </nav>
     </div>
 </body>
-
 </html>
