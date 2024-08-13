380e61ee5706f7a31121a4c6e67731e5f5710ea0
Rosalyn Wimberly <rosalynwimberly@gmail.com>
2024-08-13 18:12:48 -0400

    feat: Add images for carousel

--- index.php
+++ index.php
@@ -1,23 +1,14 @@
 <?php include "includes/header.php"; ?>
 
 <main>
-
-        <h2>Content</h2>
-        <div class="hero-image">
-
+    <div class="gallery">
+        <div class="gallery-container">
+            <img class="gallery-item gallery-item-1" src="" data-index="1"/>
+            <img class="gallery-item gallery-item-1" src="" data-index="1"/>
+            <img class="gallery-item gallery-item-1" src="" data-index="1"/>
         </div>
-        <div>
-        <form action="contact.php" method="post">
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-            <label for="message">Message:</label>
-            <textarea id="message" name="message" required></textarea>
-            <button type="submit">Send</button>
-        </form>
+
     </div>
-    
 </main>
 
 <?php include "includes/footer.php"; ?>
